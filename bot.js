2c10316583bee0e38124270d43280d0f8e6efb9b
botuliambr <33078224+botuliambr@users.noreply.github.com>
2017-10-27 17:53:21 -0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,14 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
+client.on('message', (message) => {
+  console.log(message.channel.id);
+  if(message.channel.id.indexOf('channel.isPrivate') == 1) {
+    msg.author.sendMessage("``` [ O BOT SÓ FUNCIONA NO GRUPO ]```")
+    return;
+  } 
+ });
+
 client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
